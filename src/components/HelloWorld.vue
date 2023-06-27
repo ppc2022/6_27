d02182b3650acdb06b4feb72e0040f5f2460c69c
Ppc2022 <1750644274@qq.com>
2023-06-27 14:32:27 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+const isactive = ref(false);
+function go() {
+  isactive.value = !isactive.value;
+  console.log(isactive.value);
+}
+
+// return { isactive };
+</script>
+
+<template>
+  <div class="main" :class="{ active: isactive }">
+    <el-button type="info" @click="go" style="float: right">go</el-button>
+  </div>
+</template>
+
+<style scoped>
+.active {
+  transform: translateX(-90%);
+}
+.main {
+  width: 300px;
+  height: 300px;
+  position: fixed;
+  top: 300px;
+  left: 0;
+  background-color: antiquewhite;
+  transition: all 0.3s ease;
+}
+</style>
